a61f0f04bb56b40c091ea0b9f6b556deee6d7fcb
surfblink <surfblink.repo@gmail.com>
2026-01-23 17:41:16 +0000

    remove multiple char+ in ARM build

--- arm/words/cell-plus.s
+++ arm/words/cell-plus.s
@@ -2,8 +2,3 @@ CODEWORD "cell+", CELLPLUS @ ( x -- x+4 )
   add tos, #cellsize
 NEXT
 
-# -----------------------------------------------------------------------------
-  CODEWORD "char+", CHARPLUS @ ( x -- x+1 ) STACK: Add one to TOS
-# -----------------------------------------------------------------------------
-  add tos, #1 
-NEXT
