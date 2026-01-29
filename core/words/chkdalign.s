166588d4167314db3bde2a17aa91c2405f031edc
surfblink <surfblink.repo@gmail.com>
2026-01-29 11:25:28 +0000

    alignment guards

---
+++ core/words/chkdalign.s
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-3.0-only
+/*
+WORD:  chkdalign
+STACK: ( -- ) 
+MOTIF: 
+CATEG: system
+STDID: 
+SHORT: check DP for cell alignment, throw exception (-9) if not 
+*/
+
+COLON "chkdalign", CHKDALIGN 
+	.word XT_DP
+	.word XT_DUP
+	.word XT_ALIGNED
+	.word XT_MINUS
+	.word XT_DOCONDBRANCH,CHKDALIGN_0001 # if
+	.word XT_DOLITERAL
+	.word -9
+	.word XT_THROW
+CHKDALIGN_0001: # then
+	.word XT_EXIT
