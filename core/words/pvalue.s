88d44c7545b91b579a9f09e846056a0e10975825
Martin Kobetic <magh@kobetic.ca>
2026-02-06 22:20:49 -0500

    tweaks

--- core/words/pvalue.s
+++ core/words/pvalue.s
@@ -3,15 +3,13 @@
 
 \ Updating a pvalue means writing a new pvalue record in the current pvarena,
 \ and then updating the corresponding RAM cell with the same value.
-: pvstore ( x addr -- ) \ update pvalue with RAM address addr to value x
-    \ check that there is room in the current arena, throw otherwise
-    pvarena pvasize + pvp <= if
-        -8 throw \ TODO: -8 is dictionary overflow, probably want something better here
-    then
+: pv.store ( x addr -- ) \ update pvalue with RAM address addr to value x
+    \ check that there is room in the current arena, compact and swap arenas otherwise
+    pvarena pvarena.size + pvp <= if pvarena.swap then
     \ write pvalue RAM address as the record ID
-    dup pvp !df \ store addr at pvp
+    dup pvp !pvf \ store addr at pvp
     over pvp cell+ tuck \ ( x addr pvp+ x pvp+ )
-    !df \ store x at pvp+
+    !pvf \ store x at pvp+
     cell+ to pvp \ update pvp to pvp++
     ! \ update ram
 ;
@@ -22,15 +20,15 @@
 \ TODO: This is just for testing in the interpreter,
 \ ITC will want to call PV_STORE with literal XTs of predefined flash values.
 : pvto ( x "name" -- ) \ set value to x and write pvalue record for it
-    vaddr pvstore ;
+    vaddr pv.store ;
 
 \ Arena state is stored in the first record of the arena.
 \ First cell is a counter with MSB set (so that it doesn't match any real RAM address).
 \ Second cell is either -1 or 0. Erased dormant arena has both cells set to -1.
 \ Current arena is the one with higher ID that is not -1.
 : pvarena.init ( -- ) \ set pvarena to whichever arena should be current
-    pvarena1 @df dup 1+ if \ is arena1 dormant? (ID = -1)
-        pvarena2 @df dup 1+ if \ is arena2 dormant?
+    pvarena1 @pvf dup 1+ if \ is arena1 dormant? (ID = -1)
+        pvarena2 @pvf dup 1+ if \ is arena2 dormant?
             \ (a1id a2id) higher ID wins
             < if pvarena2 to pvarena
             else pvarena1 to pvarena
@@ -40,12 +38,12 @@
             2drop \ unused arena IDs
         then
     else \ arena1 is dormant ( -1 )
-        pvarena2 @df 1+ if \ is arena2 dormant?
+        pvarena2 @pvf 1+ if \ is arena2 dormant?
             pvarena2 to pvarena
         else \ arena2 is dormant
             \ both arenas are blank, initialize arena1 and use it.
-            $80000000 pvarena1 !df
-            0 pvarena1 cell+ !df
+            $80000000 pvarena1 !pvf
+            0 pvarena1 cell+ !pvf
             pvarena1 to pvarena
         then
         drop \ unused arena1 ID
@@ -58,9 +56,9 @@
     pvarena.init \ initialize pvarena
     pvarena cell+ cell+ \ skip the arena record
     begin
-    dup @df dup 1+ while \ if record id is not all 1 bits, i.e. -1
+    dup @pvf dup 1+ while \ if record id is not all 1 bits, i.e. -1
         \ ( pvp pv-ram )
-        swap cell+ tuck @df \ ( pvp+ pv-ram [pvp+] )
+        swap cell+ tuck @pvf \ ( pvp+ pv-ram [pvp+] )
         swap ! \ update pvalue in RAM ( pvp+ )
         cell+ \ ( pvp++ )
     repeat
@@ -73,20 +71,20 @@
 ;
 
 : pvarena.erase ( addr -- ) \ erase arena at addr from the end
-    dup pvasize + pvpgsize -
+    dup pvarena.size + pvflash.page -
     begin 2dup <= while
-        dup df.erase
-        pvpgsize -
+        dup pvflash.erase
+        pvflash.page -
     repeat
     2drop
 ;
 
-\ used to prepare dormant arena for arena swap
+\ used to prepare dormant arena for arena swap (this should be noname)
 : pv.writeword ( awp ffa - awp++ ) \ write pvalue record at awp if ffa is pvalue, advance awp
     dup @ flag.pvalue and if
         ffa2cfa >body @ \ (awp pv-ram)
-        2dup swap !df @ \ write record ID (awp [pv-ram] )
-        swap cell+ tuck !df \ (awp+)
+        2dup swap !pvf @ \ write record ID (awp [pv-ram] )
+        swap cell+ tuck !pvf \ (awp+)
         cell+ \ (awp++)
     else drop \ drop the ffa (awp)
     then
@@ -100,14 +98,14 @@
 : pvarena.swap ( -- ) \ write fresh pvalues into the dormant arena and swap arenas
     pvarena.dormant
     \ check if it needs to be erased
-    dup cell+ dup @df 1+ 0<> if dup pvarena.erase then
+    dup cell+ dup @pvf 1+ 0<> if dup pvarena.erase then
     \ write dirty mark ( arena arena+ )
-    dup 0 swap !df cell+ \ ( arena arena++ )
+    dup 0 swap !pvf cell+ \ ( arena arena++ )
     \ write fresh record for each persistent value in forth-wordlist
     ['] pv.writeword forth-wordlist traverse-wordlist
     swap \ swap the arena pointers ( awp arena )
     \ write arena ID (this must happen last)
-    dup pvarena @df 1+ swap !df
+    dup pvarena @pvf 1+ swap !pvf
     \ swap arenas ( awp arena )
     pvarena swap ( awp old-arena new-arena )
     to pvarena swap \ set pvarena
@@ -127,27 +125,32 @@ END FETCH_PVF
 DEFER "pvflash.erase", PVF_ERASE, XT_FAUXERASE /* ( addr -- ) erase PV flash page at addr */
 END FETCH_PVF
 
-NONAME FAUXERASE /* :noname pvpgsize $FF fill ; */
-    .word XT_PVFLASH_SIZE, XT_FF, XT_FILL, XT_EXIT
+NONAME FAUXERASE /* :noname pvflash.page $FF fill ; */
+    .word XT_PVFLASH_PAGE, XT_PVFLASH_FF, XT_FILL, XT_EXIT
 END FAUXERASE
 
 /* this is just a hack to avoid XT_DOLITERAL, 0xFF in FAUXERASE
  because it's insanely complicated to step through */
-CONSTANT "ff", FF, 0xFF 
+CONSTANT "pvflash.ff", PVFLASH_FF, 0xFF /* value of an erased byte in flash */
+END PVFLASH_FF
 
 /* pvalue memory constants */
 
-CONSTANT "pvarena1", PVARENA1, pvarena1_lower /* ( -- addr ) address of pvalue arena 1  */
+CONSTANT "pvarena1", PVARENA1, pvarena1_lower /* address of pvalue arena 1  */
 END PVARENA1
 
-CONSTANT "pvarena2", PVARENA2, pvarena2_lower /* ( -- addr ) address of pvalue arena 2  */
+CONSTANT "pvarena2", PVARENA2, pvarena2_lower /* address of pvalue arena 2  */
 END PVARENA2
 
-CONSTANT "pvarena.size", PVARENA_SIZE, pvarena.size /* ( -- u ) size of pvalue arena (multiple of flash page size) */
+CONSTANT "pvarena.size", PVARENA_SIZE, pvarena.size /* size of pvalue arena (multiple of flash page size) */
 END PVARENA_SIZE
 
-CONSTANT "pvflash.size", PVFLASH_SIZE, pvflash.page /* ( -- u ) size of arena page (flash page size) */
+CONSTANT "pvflash.page", PVFLASH_PAGE, pvflash.page /* size of PV flash page */
+END PVFLASH_PAGE
+
+CONSTANT "pvflash.size", PVFLASH_SIZE, pvflash.size /* total size of PV flash */
 END PVFLASH_SIZE
+
 
 /* pvalue runtime values, must be initialized by pv.init */
 
@@ -198,7 +201,7 @@ END PV_STORE
 
 # ----------------------------------------------------------------------
 
-COLON "vaddr", VADDR /* ( "name" -- addr ) RAM address of value "name" */
+COLON "vaddr", VADDR /* ( "name" -- addr ) RAM address of value or variable "name" */
 	.word XT_TICK
 	.word XT_TO_BODY
 	.word XT_FETCH
@@ -319,7 +322,7 @@ COLON "pvarena.erase", PVARENADOTERASE /* ( addr -- ) erase arena at addr from t
 	.word XT_DUP
 	.word XT_PVARENA_SIZE
 	.word XT_PLUS
-	.word XT_PVFLASH_SIZE
+	.word XT_PVFLASH_PAGE
 	.word XT_MINUS
 PVARENADOTERASE_0001: # begin
 	.word XT_2DUP
@@ -327,7 +330,7 @@ PVARENADOTERASE_0001: # begin
 	.word XT_DOCONDBRANCH,PVARENADOTERASE_0002
 	.word XT_DUP
 	.word XT_PVF_ERASE
-	.word XT_PVFLASH_SIZE
+	.word XT_PVFLASH_PAGE
 	.word XT_MINUS
 	.word XT_DOBRANCH,PVARENADOTERASE_0001
 PVARENADOTERASE_0002:
