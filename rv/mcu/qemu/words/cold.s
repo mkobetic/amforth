4871333b5904eb4fe108f3ebd21241e73da573b8
surfblink <surfblink.repo@gmail.com>
2026-02-25 17:29:44 +0000

    risc-v qemu virt standalone

---
+++ rv/mcu/qemu/words/cold.s
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-3.0-only
+CODEWORD "cold", COLD /* ( i*x -- )(R: j*y -- ) assembler part of the boot sequence, starting the VM */
+
+/* This is assembly part of the startup sequence that creates the basic runtime environment for the virtual machine.
+   It sets up the hardware, the stack pointers and the forth registers for the WARM word, 
+   then boots the virtual machine by jumping to the inner interpreter.
+
+   MCUs may override this to inject additional initialization at specific points.
+*/
+
+  la s5, RAM_upper_returnstack
+  la s4, RAM_upper_datastack # TW hack
+
+  la s1, XT_WARM
+
+  j DO_EXECUTE
+
+END COLD
---
+++ rv/mcu/qemu/words/env-board.s
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: GPL-3.0-only
+
+ENVIRONMENT "board", BOARD
+
+.ifdef BUILD_203
+    STRING "WCH CH32V203"
+.endif
+
+.ifdef TARGET_307
+    STRING "WCH CH32V307"
+.endif
+
+.ifdef BUILD_305
+    STRING "WCH CH32V305"
+.endif
+
+.ifdef TARGET_QEM
+    STRING "QEMU"
+.endif
+
+.word XT_EXIT
+
