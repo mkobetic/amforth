0c8a6566b1e926dd2aae8a4ab921d65b91dceeeb
Martin Kobetic <magh@kobetic.ca>
2026-04-02 09:52:44 -0400

    arch & ENV_CONSTANT

---
+++ arm/words/env-arch.s
@@ -0,0 +1,2 @@
+ENV_CONSTANT "arch", ARCH, 1 /* ( -- u ) cpu architecture, 1=ARM/2=RV */
+END ARCH
---
+++ rv/words/env-arch.s
@@ -0,0 +1,2 @@
+ENV_CONSTANT "arch", ARCH, 2 /* ( -- u ) cpu architecture, 1=ARM/2=RV */
+END ARCH
